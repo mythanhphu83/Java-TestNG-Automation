dd6ce2bda3582dd4393230a1c48a085657c1a4fb
Yen Le <yenle@kms.com.vn>
2018-08-13 15:25:21 +0700

    add example

---
+++ phpTravels/src/main/java/data/generalData.java
@@ -0,0 +1,17 @@
+package data;
+
+public class generalData {
+    public void login(){
+         String email = "";
+        String password = "";
+
+    }
+
+    public void menuHeader(){
+        String home = "home";
+    }
+
+    public void filterSeachHotel(){
+
+    }
+}

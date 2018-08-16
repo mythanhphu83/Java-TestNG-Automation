e4a732b5e01680c3499513522895032f3817b3ac
ThuyNguyen71187 <thuynguyen@kms-technology.com>
2018-08-16 13:16:38 +0700

    ThuyNguyen: - updated resultsVerify.class

--- phpTravels/src/test/java/testCases/testVerifyResults.java
+++ phpTravels/src/test/java/testCases/testVerifyResults.java
@@ -8,24 +8,28 @@ public class testVerifyResults extends GlobalVariables {
         System.setProperty("webdriver.chrome.driver",
                 "./drivers/chromedriver.exe");
         driver = new ChromeDriver();
+//        driver.get("https://www.phptravels.net/tours/search?price=&type=197&txtSearch=&searching=&modType=");
+//        driver.get("https://www.phptravels.net/flights/0/0/0/0/0/0/1/0/0/0/0/0/Emirates/Air-Madagascar");
         driver.get("https://www.phptravels.net/cars/search?stars=4&price=&pickup=yes&txtSearch=&searching=&modType=");
+
         driver.manage().window().maximize();
 
         resultsVerify checkResult = new resultsVerify();
-        commonFunctions timePage = new commonFunctions();
+
+//        checkResult.verifyTourType("Holidays");
+
+
+//        commonFunctions.sleep(longSleepTime);
+//        checkResult.verifyAirlineNameList();
+
+
+
+//        commonFunctions.sleep(longSleepTime);
+//        commonFunctions timePage = new commonFunctions();
         checkResult.verifyAirportPickup(true);
 //        checkResult.verifyAirportPickup(false);
+//        checkResult.verifyStarGrade(4);
 
-        checkResult.verifyStarGrade(4);
-;
-
-        driver.get("https://www.phptravels.net/tours/search?price=&type=197&txtSearch=&searching=&modType=");
-        timePage.sleep(20);
-        checkResult.verifyTourType("Holiday");
-
-        driver.get("https://www.phptravels.net/flights/0/0/0/0/0/0/1/0/0/0/0/0/Emirates/Air-Madagascar");
-        timePage.sleep(20);
-        checkResult.verifyAirlineNameList();
 
         driver.close();
         driver.quit();
